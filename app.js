493c1767a5de294a2c65117f52a38ba15e5355c0
Siddhesh Kamble <siddhesh.kamble7867@gmail.com>
2021-07-30 22:31:34 +0530

    btn state improv

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ btn.addEventListener('click', () => {
   loader.classList.add('D-Block');
   btn.classList.remove('W(P74)');
   btn.classList.add('W(P98)');
+  btn.classList.add('opacity-65');
   setTimeout(() => {
     loader.classList.remove('D-Block');
     loader.classList.add('D-None');
     btn.classList.add('W(P74)');
     btn.classList.remove('W(P98)');
+    btn.classList.remove('opacity-65');
   }, 2000);
 });
